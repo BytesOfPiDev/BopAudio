3e5386bb2d76c4d2918989d4f9bf46efe980d01a
Andre Mitchell <andre.mitchell@bytesofpi.com>
2024-04-25 18:05:14 -0400

    Remove SteamAudio cmake vars
    Signed-off-by: Andre Mitchell <andre.mitchell@bytesofpi.com>

--- Code/Platform/Linux/PAL_linux.cmake
+++ Code/Platform/Linux/PAL_linux.cmake
@@ -2,14 +2,3 @@
 set(PAL_TRAIT_BOPAUDIO_SUPPORTED TRUE)
 set(PAL_TRAIT_BOPAUDIO_TEST_SUPPORTED TRUE)
 set(PAL_TRAIT_BOPAUDIO_EDITOR_TEST_SUPPORTED TRUE)
-
-if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-  set(STEAMAUDIO_LIBS ${gem_path}/3rdParty/SteamAudio/steamaudio_4.4.1/steamaudio/lib/linux-x86/libphonon.so)
-else()
-  set(STEAMAUDIO_LIBS ${gem_path}/3rdParty/SteamAudio/steamaudio_4.4.1/steamaudio/lib/linux-x64/libphonon.so)
-
-  set(STEAMAUDIO_DLLS
-  ${gem_path}/3rdParty/SteamAudio/steamaudio_4.4.1/steamaudio/lib/linux-x64/libphonon.so
-)
-endif()
-
